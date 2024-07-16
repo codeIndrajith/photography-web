580511838e1edb81b78e53ba18d1d8e9676c9288
Indrajith <141928104+codeIndrajith@users.noreply.github.com>
2024-07-16 23:18:19 +0530

    Add login functionality to ph club

--- frontend/src/screens/PhLoginScreen.jsx
+++ frontend/src/screens/PhLoginScreen.jsx
@@ -1,45 +1,68 @@
 import React, { useState } from 'react';
+import './CSS/Ph-login.css';
+import { useLoginPhClubMutation } from '../slices/phClubApiSlices';
+import { setCredentials } from '../slices/authPhClub';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
 const PhLoginScreen = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const dispatch = useDispatch();
+
+  const [loginPhClub, isLoading] = useLoginPhClubMutation();
 
   // handle submit function
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
+
+    if (!name || !email) {
+      toast.error('Add details');
+    } else {
+      try {
+        const res = await loginPhClub(name, email).unwrap();
+        dispatch(setCredentials({ ...res }));
+        toast.success('Login success');
+        setName('');
+        setEmail('');
+      } catch (error) {
+        toast.error(error);
+      }
+    }
   };
   return (
-    <div>
-      <div>
-        <div>
-          <h2>image</h2>
-        </div>
-        <div>
+    <div className="login-container">
+      <div className="login-card">
+        <div className="login-header">
           <h2>Sign in Account</h2>
-          <form onSubmit={submitHandler}>
-            <div>
-              <label>Email</label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Enter Name"
-              />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter Name"
-              />
-            </div>
-            <div>
-              <button type="submit">Login</button>
-            </div>
-          </form>
         </div>
+        <form className="login-form" onSubmit={submitHandler}>
+          <div>
+            <label className="login-label">Email</label>
+            <input
+              type="text"
+              className="login-input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder="Enter Name"
+            />
+          </div>
+          <div>
+            <label className="login-label">Password</label>
+            <input
+              type="text"
+              className="login-input"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter Name"
+            />
+          </div>
+          <div>
+            <button type="submit" className="login-button">
+              Login
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
